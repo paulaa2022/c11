9652f137f996eeefff41f0a29bbb5f402a9c7a47
paulaa2022 <111533599+paulaa2022@users.noreply.github.com>
2022-10-03 19:33:34 -0300

    Add files via upload

---
+++ PROJECT-TEMPLATE_C11-main/sketch.js
@@ -0,0 +1,18 @@
+var barco, barcoImg;
+var sea, seaImg;
+
+function preload(){
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+    drawSprites();
+
+ 
+}
